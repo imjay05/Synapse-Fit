7e2304ea9c2ec76877ed41f7ec8810e3f4026f2e
imjay05 <imjaydigambarshelke@gmail.com>
2025-10-27 12:33:40 +0530

    activityServices

---
+++ Backend/activityService/src/main/java/com/fitness/activityService/model/ActivityType.java
@@ -0,0 +1,13 @@
+package main.java.com.fitness.activityService.model;
+
+public enum ActivityType {
+    RUNNING,
+    WALKING,
+    CYCLING,
+    WEIGHT_TRAINING,
+    YOGA,
+    HIIT,
+    CARDIO,
+    STRETCHING,
+    OTHER
+}
